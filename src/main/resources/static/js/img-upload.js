800965870bfa3e9172db8edcea0e7a5eb6393e18
xp541130126 <541130126@qq.com>
2017-08-06 16:50:54 +0800

    完成头像裁剪功能

---
+++ src/main/resources/static/js/img-upload.js
@@ -0,0 +1,76 @@
+/**
+ * Created by xp on 2017/8/6.
+ */
+$(document).ready(function(){
+    'use strict';
+    // 初始化
+    var $image =  $('#image');
+    $image.cropper({
+        aspectRatio: 16 / 9,
+        preview: '.img-preview',
+        zoomOnWheel: false,
+    });
+
+    // 事件代理绑定事件
+    $('.img-btn-grp').on('click', '[data-method]', function() {
+        var $this = $(this);
+        var data = $this.data();
+        var result = $image.cropper(data.method, data.option, data.secondOption);
+
+        switch (data.method) {
+
+            case 'getCroppedCanvas':
+                if (result) {
+
+                    // 显示 Modal
+                    $('#cropped-modal').modal().find('.am-modal-bd').html(result);
+                    $('#download').attr('href', result.toDataURL('image/jpeg'));
+                }
+                break;
+        }
+    });
+    // 上传图片
+    var $inputImage = $('#inputImage');
+    var URL = window.URL || window.webkitURL;
+    var blobURL;
+
+    if (URL) {
+        $inputImage.change(function () {
+            var files = this.files;
+            var file;
+
+            if (files && files.length) {
+                file = files[0];
+
+                if (/^image\/\w+$/.test(file.type)) {
+                    blobURL = URL.createObjectURL(file);
+                    $image.one('built.cropper', function () {
+
+                        // Revoke when load complete
+                        URL.revokeObjectURL(blobURL);
+                    }).cropper('reset').cropper('replace', blobURL);
+                    $inputImage.val('');
+                } else {
+                    window.alert('Please choose an image file.');
+                }
+            }
+
+            // Amazi UI 上传文件显示代码
+            var fileNames = '';
+            $.each(this.files, function() {
+                fileNames += '<span class="am-badge">' + this.name + '</span> ';
+            });
+            $('#file-list').html(fileNames);
+        });
+    } else {
+        $inputImage.prop('disabled', true).parent().addClass('disabled');
+    }
+});
+function selectFile() {
+    //触发file click事件
+    $("#inputImage").trigger("click");
+}
+function reset() {
+    var $image =  $('#image');
+    $image.cropper('reset')
+}
